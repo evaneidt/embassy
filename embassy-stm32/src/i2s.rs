bf8a76daa2d99596a67623ac46c178366b6bd95b
E-Eidt <evaneidt@gmail.com>
2025-02-22 14:31:04 -0500

    Trying to make rxonly no mck

--- embassy-stm32/src/i2s.rs
+++ embassy-stm32/src/i2s.rs
@@ -315,6 +315,32 @@ impl<'d, W: Word> I2S<'d, W> {
         )
     }
 
+    /// Create a receiver driver without a master clock pin.
+    pub fn new_rxonly_nomck<T: Instance>(
+        peri: impl Peripheral<P = T> + 'd,
+        sd: impl Peripheral<P = impl MisoPin<T>> + 'd,
+        ws: impl Peripheral<P = impl WsPin<T>> + 'd,
+        ck: impl Peripheral<P = impl CkPin<T>> + 'd,
+        rxdma: impl Peripheral<P = impl RxDma<T>> + 'd,
+        rxdma_buf: &'d mut [W],
+        freq: Hertz,
+        config: Config,
+    ) -> Self {
+        Self::new_inner(
+            peri,
+            None,
+            new_pin!(sd, AfType::output(OutputType::PushPull, Speed::VeryHigh)),
+            ws,
+            ck,
+            None,
+            None,
+            new_dma!(rxdma).map(|d| (d, rxdma_buf)),
+            freq,
+            config,
+            Function::Receive,
+        )
+    }
+
     #[cfg(spi_v3)]
     /// Create a full duplex driver.
     pub fn new_full_duplex<T: Instance>(
